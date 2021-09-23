109fbf853ed92d868d8e4e71742ea0b50f988e31
Aya Mohsen <69248543+aya240@users.noreply.github.com>
2021-09-23 15:24:56 -0700

    Add files via upload

---
+++ 1/js/j.js
@@ -0,0 +1,32 @@
+
+
+if( window.outerWidth <=  799){
+    
+     document.getElementById('nav').style.display = "none"; 
+
+                                  
+                      }
+
+if( window.outerWidth >  799){
+    
+     document.getElementById('ii').style.display = "none"; 
+
+                                  
+                      }
+
+function fun(){
+    "use strict";
+    document.getElementById('na').classList.toggle('vi');
+    
+                                  
+                      }
+
+
+
+function fu(){
+    "use strict";
+    document.getElementById('se').classList.toggle('vi');
+    
+                                  
+                      }
+
